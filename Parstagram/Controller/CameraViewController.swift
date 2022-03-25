20fcad8da298560be9808515195c3920e536e1f3
Shoaib Huq <slimsho123@gmail.com>
2022-03-25 17:55:36 -0500

    added TableView

--- Parstagram/Controller/CameraViewController.swift
+++ Parstagram/Controller/CameraViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -18,6 +20,25 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func submitButtonPressed(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()
+        
+        let imageData = imageView.image?.pngData()
+        
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground() { (success, error) in
+            if success {
+                print("saved")
+            } else {
+                print("error saving image")
+            }
+        }
+        
     }
     
     @IBAction func openCamera(_ sender: Any) {
@@ -36,6 +57,18 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true
+                , completion: nil)
+        
+    }
     
     /*
     // MARK: - Navigation
--- Parstagram/Controller/FeedViewController.swift
+++ Parstagram/Controller/FeedViewController.swift
@@ -6,18 +6,55 @@
 //
 
 import UIKit
+import Parse
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
     
-    @IBAction func cameraButtomPressed(_ sender: Any) {
+    override func viewDidAppear(_ animated: Bool) {
         
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        query.findObjectsInBackground{(posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell") as! PostTableViewCell
+        
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.authorLabel.text = user.username
+        
+        cell.commentLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)
+        cell.photoView.af.setImage(withURL: url!)
+        
+        return cell
     }
     
 
